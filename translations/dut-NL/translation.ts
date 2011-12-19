662d0d997a3d6dcc43ef88c546562e666669b180
svda <sander@myxt.nl>
2011-12-19 20:20:33 +0100

    Added Dutch translation.

---
+++ translations/dut-NL/translation.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="nl_NL">
+<context>
+    <name>extension/recaptcha</name>
+    <message>
+        <location filename="recaptchatype.php" line="55"/>
+        <location filename="recaptchatype.php" line="63"/>
+        <source>The reCAPTCHA wasn&apos;t entered correctly. Please try again.</source>
+        <translation>De ingetypte woorden zijn onjuist. Probeer het opnieuw.</translation>
+    </message>
+</context>
+</TS>

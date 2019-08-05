2c5c64f8b763211357787e4fcac63aa9e3d43096
Ruben Bratsberg <rb@aplia.no>
2019-08-05 11:39:28 +0200

    Added Norwegian translation of error message

---
+++ translations/nor-NO/translation.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>extension/recaptcha</name>
+    <message>
+        <location filename="recaptchatype.php" line="55"/>
+        <location filename="recaptchatype.php" line="63"/>
+        <source>reCAPTCHA ble ikke skrevet inn korrekt. Prøv igjen.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
